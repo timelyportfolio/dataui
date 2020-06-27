84a4e9a59b494304dae9813d5a125824ed9dd375
timelyportfolio <kent.russell@timelyportfolio.com>
2020-06-27 17:01:47 -0500

    add htmltools:: to htmlDependency

--- R/dependency.R
+++ R/dependency.R
@@ -1,10 +1,10 @@
 
-#' Title
+#' 'JavaScript' Dependencies for Standalone Usage
 #'
 #' @return
 #' @export
 html_dependency_dataui <- function() {
-  htmlDependency(
+  htmltools::htmlDependency(
     name = 'data-ui',
     version = '0.8.4',
     src = c(file = system.file("www", package="dataui")),
